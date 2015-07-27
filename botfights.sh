9151917f6ab763952d6e0a6b70921e618e4f4d03
Colin M. Saunders <colinmsaunders@gmail.com>
2015-07-26 22:04:28 -0700

    botfights.sh

---
+++ botfights.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# botfights.sh -- wrapper for botfights.io <=> monkeystud
+# either verify a bot, or 
+# run a botfight
+
+if [ $1='verify' ];
+    then
+    python monkeystud.py verify $2
+elif [ $1='fight' ];
+    then
+    shift
+    python monkeystud.py tournament 10000 $@
+fi
--- fight.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# fight.sh -- wrapper for botfights.io <=> monkeystud
-# takes a list of bots on command line
-
-python monkeystud.py tournament 10000 $@
