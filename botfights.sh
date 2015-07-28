fcc0d1604228dca7d200643bc9dfa58479e2f7d3
Colin M. Saunders <colins@surveymonkey.com>
2015-07-28 11:12:21 -0700

    spaces

--- botfights.sh
+++ botfights.sh
@@ -4,10 +4,10 @@
 # either verify a bot, or
 # run a botfight
 
-if [ $1='verify' ];
+if [ $1 = 'verify' ];
     then
     python monkeystud.py verify $2
-elif [ $1='fight' ];
+elif [ $1 = 'fight' ];
     then
     shift
     python monkeystud.py tournament 1000 $@
