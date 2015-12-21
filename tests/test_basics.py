f0bc880cf5ab757b16cb5fce23c3cffa18c7c631
John Anderson <sontek@gmail.com>
2015-12-21 08:59:12 -0800

    Added a test for antes

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -9,7 +9,6 @@ def test_play_hand_valid_argument():
 
     def get_play(history):
         history_ref = history
-        print(history)
         return 'Z'
 
     player1.get_play = get_play
@@ -20,5 +19,37 @@ def test_play_hand_valid_argument():
         player1.done()
         player2.done()
 
+    assert player1.played is True
+    assert player2.played is True
+
+
+@pytest.mark.unit
+def test_play_game_ante():
+    from monkeystud import play_game, make_player
+    player1 = make_player('p_1', 'p_random', False)
+    history_ref = None
+
+    def get_play(history):
+        history_ref = history
+        return 'C'
+
+    player1.get_play = get_play
+    player2 = make_player('p_2', 'p_random', False)
+
+
+    try:
+        with mock.patch('monkeystud.logging') as l:
+            play_game([player1, player2], False)
+    finally:
+        player1.done()
+        player2.done()
+
+    antes = []
+    for call in l.debug.mock_calls:
+        data = call[1][0]
+        if 'antes' in data:
+            antes.append(int(data.split()[-1]))
+
+    assert antes[0] < antes[-1]
     assert player1.played is True
     assert player2.played is True
